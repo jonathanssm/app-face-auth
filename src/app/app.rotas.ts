b04aacc30c4bf16173428716a024dd6393b207a2
Jonathan Sebastian <33239505+jonathanssm@users.noreply.github.com>
2020-09-08 17:49:03 -0300

    Paginas iniciais

--- src/app/app.rotas.ts
+++ src/app/app.rotas.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 
 // Componentes
 import { HomeComponent } from './paginas/home/home.component';
+import { CadastroComponent } from './paginas/cadastro/cadastro.component';
+import { AutenticarComponent } from './paginas/autenticar/autenticar.component';
 
 export const ROTAS: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, data: { title: 'Home' } },
+    { path: 'cadastro', component: CadastroComponent, data: { title: 'Cadastro' } },
+    { path: 'autenticar', component: AutenticarComponent, data: { title: 'Autenticar' } }
 ];
